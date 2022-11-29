c8c6198d663c4368dd377d685f72dda3d40e1430
Casey Wittrock <caseywittrockmn@gmail.com>
2022-11-28 21:54:23 -0600

    writing javascript functions

--- calculator.js
+++ calculator.js
@@ -21,17 +21,28 @@ function getCurrentUIValues() {
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
+  document.getElementById('loan-amount').value = (Math.floor(Math.random() * 100) * 300);
+  document.getElementById('loan-years').value = Math.floor((Math.random() * 30) + 1);
+  document.getElementById('loan-rate').value = (Math.round(Math.random() * 3200) / 100);
+  calculateMonthlyPayment(getCurrentUIValues());
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+  const monthlyRate = (values.rate / 100) / 12;
+  const n = Math.floor(values.years * 12);
+  return (
+    (monthlyRate * values.amount) /
+    (1 - Math.pow((1 + monthlyRate), -n))
+  ).toFixed(2);
 }
 
 // Given a string representing the monthly payment value,
